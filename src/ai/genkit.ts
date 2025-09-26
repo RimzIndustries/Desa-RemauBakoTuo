d77c331fd1a3bd949e0a5a4024b8e37d818f2f96
Rimz industries <rimzindustries@gmail.com>
2025-09-26 06:12:03 +0000

    Try fixing this error: `Runtime Error: TypeError: (0 , {imported module

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,8 @@
-import {genkit} from '@genkit-ai/ai';
-import {googleAI} from '@genkit-ai/googleai';
+import { googleAI } from '@genkit-ai/googleai';
+import * as genkit from '@genkit-ai/ai';
 
 // This is the primary entrypoint for Genkit.
 // It is required that this file is imported before any other Genkit files.
-export const ai = genkit({
+export const ai = genkit.genkit({
   plugins: [googleAI()],
 });
