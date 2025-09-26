575e64e9ff917f4b3cbd3cf4fc8baff522751b46
Rimz industries <rimzindustries@gmail.com>
2025-09-26 03:52:17 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../index.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
