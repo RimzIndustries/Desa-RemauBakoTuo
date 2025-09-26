62554b69fb7909804abb0289221c879f276a5dcf
Rimz industries <rimzindustries@gmail.com>
2025-09-26 05:53:52 +0000

    Error: ./src/ai/genkit.ts:3:1 [1m[31mModule not found[39m[22m: Can't

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,7 @@
+
 import {genkit} from '@genkit-ai/next';
 import {googleAI} from '@genkit-ai/googleai';
-import {firebase} from '@genkit-ai/firebase/plugin';
+import {firebase} from '@genkit-ai/firebase';
 import { GoogleAuth } from 'google-auth-library';
 import {ai, definePrompt} from '@genkit-ai/ai';
 
@@ -19,16 +20,6 @@ genkit({
       }),
     }),
     googleAI(),
-    definePrompt({
-      name: 'summarizer',
-      input: {
-        schema: {
-          text: 'string',
-        },
-      },
-      prompt: 'Summarize the following text for me: {{text}}',
-      model: 'googleai/gemini-1.5-flash',
-    }),
   ],
   logLevel: 'debug',
   enableTracingAndMetrics: true,
