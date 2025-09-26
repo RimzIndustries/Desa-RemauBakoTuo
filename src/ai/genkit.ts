5e21c22edb3179396c4ab22dbcdafc4ee8ce485d
Rimz industries <rimzindustries@gmail.com>
2025-09-26 06:19:52 +0000

    dell

--- src/ai/genkit.ts
+++
@@ -1,8 +0,0 @@
-import { genkit } from '@genkit-ai/ai';
-import { googleAI } from '@genkit-ai/googleai';
-
-// This is the primary entrypoint for Genkit.
-// It is required that this file is imported before any other Genkit files.
-export const ai = genkit({
-  plugins: [googleAI()],
-});
