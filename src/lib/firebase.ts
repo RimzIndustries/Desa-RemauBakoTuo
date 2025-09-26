19a5c978e0a142024ff0a32918ce80f748bdbf4d
Rimz industries <rimzindustries@gmail.com>
2025-09-26 07:45:32 +0000

    // Import the functions you need from the SDKs you need import { initial

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnMTKp1hTlN8Nk_zLM_nCK88-2kMBBXOI",
+  authDomain: "data-desaspasial.firebaseapp.com",
+  projectId: "data-desaspasial",
+  storageBucket: "data-desaspasial.appspot.com",
+  messagingSenderId: "51876681975",
+  appId: "1:51876681975:web:e901917b632ac97be6dbf2"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
