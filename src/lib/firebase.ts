3a4bbb52a0763561ee0fa0f713bc9abe7a61f7eb
Rimz industries <rimzindustries@gmail.com>
2025-10-01 04:22:08 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAdABNAGQFEdrFIXKZOoqyV9RCSE-ezZrM",
   authDomain: "desa-remaubakotuodata.firebaseapp.com",
   projectId: "desa-remaubakotuodata",
-  storageBucket: "desa-remaubakotuodata.appspot.com",
+  storageBucket: "desa-remaubakotuodata.firebasestorage.app",
   messagingSenderId: "903771506425",
   appId: "1:903771506425:web:41d40e071c258e90dec428"
 };
